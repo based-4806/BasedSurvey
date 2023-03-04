d33ded0a62a9eea3c27b04f27b053fb01dc27710
DavidHaighton <DavidHaighton@cmail.carleton.ca>
2023-03-03 22:29:45 -0500

    re-added multiplechoice test

---
+++ src/test/java/com/based/basedsurvey/data/MultiplechoiceQuestionTest.java
@@ -0,0 +1,64 @@
+package com.based.basedsurvey.data;
+
+import com.based.basedsurvey.repo.QuestionRepository;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class MultiplechoiceQuestionTest {
+    @Autowired
+    private QuestionRepository repo;
+
+    private MultiplechoiceQuestion question;
+    private MultiplechoiceQuestion questionUnderTest;
+
+    @Before
+    public void setup(){
+        repo.deleteAll();
+        question = new MultiplechoiceQuestion();
+        question.setPrompt("Prompt Text");
+        var options = question.getOptions();
+        options.add("option 1");
+        options.add("option 2");
+        options.add("option 3");
+        var responses = question.getResponses();
+        responses.add(0);
+        responses.add(0);
+        repo.save(question);
+        questionUnderTest = (MultiplechoiceQuestion)repo.findById(question.getId());
+    }
+
+    @After
+    public void close(){
+        repo.deleteAll();
+    }
+
+    @Test
+    public void testPrompt(){
+        assertEquals("Prompt Text", questionUnderTest.getPrompt());
+    }
+
+    @Test
+    public void testOptions(){
+        assertEquals(3, questionUnderTest.getOptions().size());
+        assertEquals("option 1", questionUnderTest.getOptions().get(0));
+        assertEquals("option 2", questionUnderTest.getOptions().get(1));
+        assertEquals("option 3", questionUnderTest.getOptions().get(2));
+    }
+    @Test
+    public void testResponses(){
+        assertEquals(2, questionUnderTest.getResponses().size());
+        assertTrue(0 == questionUnderTest.getResponses().get(0));
+        assertTrue(0 == questionUnderTest.getResponses().get(1));
+    }
+
+
+}
