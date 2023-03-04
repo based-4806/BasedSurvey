4d5ac80e81e2259403388357d450e55f3ee29fe2
DavidHaighton <DavidHaighton@cmail.carleton.ca>
2023-03-03 22:02:12 -0500

    initial commit

---
+++ src/main/java/com/based/basedsurvey/BasedSurveyApplication.java
@@ -0,0 +1,13 @@
+package com.based.basedsurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasedSurveyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasedSurveyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/based/basedsurvey/BasedSurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.based.basedsurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasedSurveyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
