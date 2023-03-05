ed0d3b54577179e9a553672f06e156a852984817
DavidHaighton <DavidHaighton@cmail.carleton.ca>
2023-03-05 14:28:19 -0500

    fixed other orm classes for testing

--- src/main/java/com/based/basedsurvey/data/OpenAnswerQuestion.java
+++ src/main/java/com/based/basedsurvey/data/OpenAnswerQuestion.java
@@ -2,8 +2,10 @@ package com.based.basedsurvey.data;
 
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +14,9 @@ import java.util.List;
 @Entity
 @Data
 public class OpenAnswerQuestion extends Question{
-    @ElementCollection
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ElementCollection(fetch = FetchType.EAGER)
     private List<String> responses = new ArrayList<>();
 
 }
