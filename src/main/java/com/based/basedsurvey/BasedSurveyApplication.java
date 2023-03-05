77be536af8f8b4a17b9318d23e7382444d270b81
Shashaank5 <shankisri@gmail.com>
2023-03-04 21:33:12 -0500

    Added barebones table to index

--- src/main/java/com/based/basedsurvey/BasedSurveyApplication.java
+++ src/main/java/com/based/basedsurvey/BasedSurveyApplication.java
@@ -1,10 +1,20 @@
 package com.based.basedsurvey;
 
+import com.based.basedsurvey.data.Survey;
+import com.based.basedsurvey.repo.SurveyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BasedSurveyApplication {
+
+    private SurveyRepository repo;
+
+    @Autowired
+    public BasedSurveyApplication(SurveyRepository repo){
+        this.repo = repo;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BasedSurveyApplication.class, args);
