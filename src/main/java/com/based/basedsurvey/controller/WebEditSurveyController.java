dfdf6ec3ba9124c69c58c41467fde97a590b821d
mack-wallace <mackwallacebusiness@gmail.com>
2023-03-06 20:51:18 -0500

    adding html and controller to edit surver

---
+++ src/main/java/com/based/basedsurvey/controller/WebEditSurveyController.java
@@ -0,0 +1,45 @@
+package com.based.basedsurvey.controller;
+
+import com.based.basedsurvey.data.Survey;
+import com.based.basedsurvey.repo.QuestionRepository;
+import com.based.basedsurvey.repo.SurveyRepository;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Log
+@Controller
+public class WebEditSurveyController {
+
+    SurveyRepository surveyRepository;
+    QuestionRepository questionRepository;
+    @Autowired
+    public WebEditSurveyController(SurveyRepository surveyRepository, QuestionRepository questionRepository){
+        this.surveyRepository = surveyRepository;
+        this.questionRepository = questionRepository;
+
+    }
+
+    @GetMapping(path = "survey/{surveyId}/edit")
+    public String homePage(Model model) {
+        model.addAttribute("surveys", surveyRepository.findAll());
+        return "index";
+    }
+
+    @PostMapping("survey/create")
+    public String createSurvey(@RequestParam String name) {
+        surveyRepository.save(new Survey(name));
+        return "redirect:/";
+    }
+
+    @PostMapping("survey/delete")
+    public String deleteQuestion(@RequestParam Long id) {
+        surveyRepository.deleteById(id);
+        return "redirect:/survey/"+id +"/edit";
+    }
+}
