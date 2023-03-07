391a32d3570202cda40ff987394e836ebc28f494
mack-wallace <mackwallacebusiness@gmail.com>
2023-03-07 17:33:48 -0500

    adding html and controller to edit survey

--- src/main/java/com/based/basedsurvey/controller/WebEditSurveyController.java
+++ src/main/java/com/based/basedsurvey/controller/WebEditSurveyController.java
@@ -1,23 +1,24 @@
 package com.based.basedsurvey.controller;
 
+import com.based.basedsurvey.data.MultiplechoiceQuestion;
+import com.based.basedsurvey.data.Question;
+import com.based.basedsurvey.data.QuestionTypes;
 import com.based.basedsurvey.data.Survey;
 import com.based.basedsurvey.repo.QuestionRepository;
 import com.based.basedsurvey.repo.SurveyRepository;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Log
 @Controller
 public class WebEditSurveyController {
 
-    SurveyRepository surveyRepository;
-    QuestionRepository questionRepository;
+    private SurveyRepository surveyRepository;
+    private QuestionRepository questionRepository;
     @Autowired
     public WebEditSurveyController(SurveyRepository surveyRepository, QuestionRepository questionRepository){
         this.surveyRepository = surveyRepository;
@@ -26,20 +27,57 @@ public class WebEditSurveyController {
     }
 
     @GetMapping(path = "survey/{surveyId}/edit")
-    public String homePage(Model model) {
-        model.addAttribute("surveys", surveyRepository.findAll());
-        return "index";
+    public String editSurvey(@PathVariable("surveyId") long id, Model model) {
+        var survey = getSurvey(id);
+        model.addAttribute("questions", questionRepository.findAllBySurveyId(survey.getId()));
+        model.addAttribute("survey", survey);
+        return "newSurvey";
     }
 
-    @PostMapping("survey/create")
-    public String createSurvey(@RequestParam String name) {
-        surveyRepository.save(new Survey(name));
-        return "redirect:/";
+    @PostMapping("question/create")
+    public String createQuestion(@RequestParam long surveyID, @RequestParam QuestionTypes qt, @RequestParam String prompt) {
+        var question = QuestionTypes.makeQuestionFromType(qt);
+        var survey = getSurvey(surveyID);
+        question.setSurvey(survey);
+        question.setPrompt(prompt);
+        questionRepository.save(question);
+        survey.getQuestions().add(question);
+        surveyRepository.save(survey);
+        return "redirect:/survey/"+surveyID +"/edit";
     }
 
-    @PostMapping("survey/delete")
-    public String deleteQuestion(@RequestParam Long id) {
-        surveyRepository.deleteById(id);
+    @PostMapping("question/delete")
+    public String deleteQuestion(@RequestParam long questionID, @RequestParam long surveyID) {
+        var survey = getSurvey(surveyID);
+        var question = getQuestion(questionID);
+        survey.getQuestions().remove(question);
+        surveyRepository.save(survey);
+        return "redirect:/survey/"+surveyID +"/edit";
+    }
+
+    @PostMapping("survey/rename")
+    public String editName(@RequestParam String prompt, @RequestParam long id) {
+        var survey = getSurvey(id);
+        survey.setName(prompt);
+        surveyRepository.save(survey);
         return "redirect:/survey/"+id +"/edit";
+    }
+
+    private Survey getSurvey(long id){
+        var survey = surveyRepository.findSurveyById(id);
+        if(survey == null){
+            log.warning("Survey with ID: "+id+" does not exist");
+            throw new ResourceNotFoundException();
+        }
+        return survey;
+    }
+
+    private Question getQuestion(long id){
+        var question = questionRepository.findById(id);
+        if(question == null){
+            log.warning("Question with ID: "+id+" does not exist");
+            throw new ResourceNotFoundException();
+        }
+        return question;
     }
 }
