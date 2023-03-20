89a5ddc6053c60af7e084dcea68070c5cb5d4675
dankvid <dankvid@pop-os.localdomain>
2023-03-20 16:33:04 -0400

    added htmx to delete on home page, added tests too

--- src/main/java/com/based/basedsurvey/controller/WebSurveyController.java
+++ src/main/java/com/based/basedsurvey/controller/WebSurveyController.java
@@ -7,10 +7,7 @@ import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Log
 @Controller
@@ -42,4 +39,12 @@ public class WebSurveyController {
         surveyRepository.deleteById(id);
         return "redirect:/";
     }
+
+    @DeleteMapping("survey/{id}")
+    @ResponseBody
+    public String deleteSurveyHtmx(@PathVariable long id){
+        surveyRepository.deleteById(id);
+        return "";
+    }
+
 }
